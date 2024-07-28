82152a9cb2b6517b51f1caf72c975f6093fbf8d4
Kwayu Mmari <mmaridenis@gmail.com>
2024-07-29 02:55:24 +0300

    fixing topheader mobile

--- lib/src/screens/TopHeader/topHeaderMobile.dart
+++ lib/src/screens/TopHeader/topHeaderMobile.dart
@@ -10,76 +10,163 @@ class TopHeaderMobile extends StatefulWidget {
   State<TopHeaderMobile> createState() => _TopHeaderMobileState();
 }
 
-class _TopHeaderMobileState extends State<TopHeaderMobile> {
+class _TopHeaderMobileState extends State<TopHeaderMobile>
+    with SingleTickerProviderStateMixin {
+  bool _isDrawerOpen = false;
+  late AnimationController _controller;
+  late Animation<Offset> _offsetAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(1.0, 0.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggleDrawer() {
+    setState(() {
+      if (_isDrawerOpen) {
+        _controller.reverse();
+      } else {
+        _controller.forward();
+      }
+      _isDrawerOpen = !_isDrawerOpen;
+    });
+  }
+
+  void _closeDrawer() {
+    setState(() {
+      _controller.reverse();
+      _isDrawerOpen = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          color: AppConst.black,
-          width: MediaQuery.of(context).size.width,
-          height: 60,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: AppConst.primary,
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: AppConst.black,
+              width: MediaQuery.of(context).size.width,
+              height: 60,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: AppConst.primary,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    AppText(
+                      txt: 'Company@domain.com',
+                      size: 15,
+                      color: AppConst.primary,
+                    ),
+                    Spacer(),
+                    Icon(
+                      FontAwesomeIcons.locationDot,
+                      color: AppConst.primary,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    AppText(
+                      txt: 'Jl. Raya Puputan \n No 142, TZ - 80234',
+                      size: 15,
+                      color: AppConst.primary,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  width: 10,
-                ),
-                AppText(
-                  txt: 'Company@domain.com',
-                  size: 15,
-                  color: AppConst.primary,
-                ),
-                Spacer(),
-                Icon(
-                  FontAwesomeIcons.locationDot,
-                  color: AppConst.primary,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                AppText(
-                  txt: 'Jl. Raya Puputan \n No 142, TZ - 80234',
-                  size: 15,
-                  color: AppConst.primary,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-              ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      'assets/logo.png',
+                      width: 200,
+                    ),
+                  ),
+                  Spacer(),
+                  MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                      onTap: _toggleDrawer,
+                      child: Icon(
+                        Icons.menu,
+                        size: 50,
+                        color: AppConst.primary,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        if (_isDrawerOpen)
+          GestureDetector(
+            onTap: _closeDrawer,
+            child: Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Stack(
+                children: [
+                  SlideTransition(
+                    position: _offsetAnimation,
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        width: 250,
+                        height: MediaQuery.of(context).size.height,
+                        color: Colors.white,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('This is a drawer-like popup'),
+                            ElevatedButton(
+                              onPressed: _closeDrawer,
+                              child: Text('Close'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset(
-                  'assets/logo.png',
-                  width: 200,
-                ),
-              ),
-              Spacer(),
-              Icon(
-                Icons.menu,
-                size: 50,
-                color: AppConst.primary,
-              ),
-            ],
-          ),
-        )
       ],
     );
   }
